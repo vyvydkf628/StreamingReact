37871629dfe8671ac43ed8bf8f21faa4106250ab
좌동철 <vyvydkf628@naver.com>
2020-07-04 19:42:19 +0900

    update streamcreate

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Modal from '../Modal';
+import history from '../../history';
+import { fetchStream, deleteStream } from '../../actions';
 
-const StreamDelete = () => {
-  return <div>StreamDelete</div>;
+const StreamDelete = (props) => {
+  const { id } = props.match.params;
+  useEffect(() => {
+    props.fetchStream(id);
+  }, []);
+
+  const actions = (
+    <React.Fragment>
+      <button  
+        onClick={() => props.deleteStream(id)} 
+        className="ui button negative"
+      >
+        Delete
+      </button>
+      <Link to="/" className="ui button">Cancel</Link>
+    </React.Fragment>
+  );
+
+  const renderContent = () => {
+    if (!props.stream) {
+      return 'Are you sure?';
+    }
+
+    return `Are you sure you want to delete title : ${props.stream.title}`;
+  }
+
+  return (
+      <Modal
+        title="Delete Stream"
+        content={renderContent()}
+        actions={actions}
+        onDismiss={() => history.push('/')}
+      />
+  );
 };
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);
--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import flv from 'flv.js';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+const StreamShow = (props) => {
+  const video = React.createRef();
+  const { id } = props.match.params;
+  let player;
+  useEffect(() => {
+    props.fetchStream(id);
+    player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    player.attachMediaElement(video.current);
+    player.load();
+  }, []);
+
+  if(!props.stream) {
+    return <div>Loading...</div>;
+  }
+
+  const {title, description} = props.stream;
+  return (
+    <div>
+      <video ref={video} style={{ width: '100%' }} controls={true} />
+      <h1>{title}</h1>
+      <h5>{description}</h5>
+    </div>
+  );
 };
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
